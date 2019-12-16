6c7f173b6679a46e733a6d7afc0004d1ff8691eb
rezarr <rezarr.business@gmail.com>
2019-12-16 19:26:00 +0700

    Initial commit

---
+++ w2monday.js
@@ -0,0 +1,53 @@
+/*
+ANCHOR WEEK 2 MONDAY
+*/
+/*
+PROBLEM 1: Newton Second Law
+Pseudocode:
+
+STORE "percepatan" with 2
+STORE "massa" with 600
+
+CALCULATE "percepatan" times "massa"
+SET "resultan" value with calculation result
+
+DISPLAY "resultan"
+*/
+/*
+PROBLEM 2: Tahun Kabisat
+Pseudocode:
+
+STORE "tahun" with any number
+
+CALCULATE "tahun" mod 4
+STORE "hasil" to the result of calculation
+IF "result" equals to 0
+    DISPLAY "tahun kabisat"
+ELSE
+    DISPLAY "bukan tahun kabisat"
+*/
+/*
+PROBLEM 3: Laundry Day
+Pseudocode:
+
+STORE "laundry" with 0
+
+WHILE "laundry" < 20
+    ADD "laundry" by 1
+
+DISPLAY "cuci sekarang!"
+*/
+/*
+PROBLEM 4: Periksa Kuku
+Pseudocode:
+
+STORE "cek" to 1
+STORE "panjang"
+
+WHILE "cek" < 40
+    DISPLAY "cek"
+    IF "cek" equals to "panjang"
+        DISPLAY "hukum!"
+    ELSE   
+        DISPLAY "puji!"
+*/
