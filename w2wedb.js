78d771df668eacb524ef7c7bf3145fa97bc1ed36
rezarr <rezarr.business@gmail.com>
2019-12-23 01:27:35 +0700

    Edited the unfinished w2wedb.js looping anchor

--- w2wedb.js
+++ w2wedb.js
@@ -1,15 +1,77 @@
-//ANCHOR WEDNESDAY:
+//ANCHOR WEEK 2 WEDNESDAY:
 //LOOPING
 
-var num = 2;
-console.log("LOOPING PERTAMA")
-while(num < 21) {
-    console.log(num + " - I love coding")
-    num += 2
-}
-var num2 = 20
-console.log("LOOPING KEDUA")
-while(num2 > 1){
-    console.log(num2 + "- I love coding")
-    num2 -= 2
-}
+//1. Melakukan Looping Menggunakan While
+
+// var num = 2;
+// console.log("LOOPING PERTAMA")
+// while(num < 21) {
+//     console.log(num + " - I love coding")
+//     num += 2
+// }
+// var num2 = 20
+// console.log("LOOPING KEDUA")
+// while(num2 > 1){
+//     console.log(num2 + "- I love coding")
+//     num2 -= 2
+// }
+
+//2. Melakukan Looping Menggunakan For
+
+// var num = 21
+
+// console.log('LOOPING PERTAMA')
+// for(i=1; i < num; i++){
+//     console.log(i + ' - I love coding')
+// }
+
+// var num2 = 0
+// console.log('LOOPING KEDUA')
+// for(j=20; j > num2; j--){
+//     console.log(j + ' - I will become fullstack developer')
+// }
+
+//3. Angka Ganjil dan Genap
+//a. Ganjil Genap
+
+// var counter = 100
+// for(i=1; i<=counter; i++){
+//     if([i] % 2 === 0){
+//         console.log(i + ' - GENAP')
+//     } else {
+//         console.log(i + ' - GANJIL')
+//     }
+// }
+
+//b. Pertambahan Counter 2 Kelipatan 3
+
+// var counter2 = 100
+// for(i=1; i<=counter2; i+=2){
+//     if([i] % 3 === 0){
+//         console.log(i + ' KELIPATAN 3')
+//     } else {
+//         console.log('')
+//     }
+// }
+
+//c. Pertambahan Counter 5 Kelipatan 6
+
+// var counter3 = 100
+// for(i=1; i<=counter3; i+=5){
+//     if([i] % 6 === 0){
+//         console.log(i + ' KELIPATAN 6')
+//     } else {
+//         console.log('')
+//     }
+// }
+
+//d. Pertambahan Counter 9 Kelipatan 10
+
+// var counter4 = 100
+// for(i=1; i<=counter4; i+=9){
+//     if([i] % 10 === 0){
+//         console.log(i + ' KELIPATAN 10')
+//     } else {
+//         console.log('')
+//     }
+// }
