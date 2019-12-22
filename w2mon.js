8f0092532cbfaee299436356082cb1ac8c6b9037
rezarr <rezarr.business@gmail.com>
2019-12-22 12:24:58 +0700

    Adding week 2 monday anchor

---
+++ w2mon.js
@@ -0,0 +1,56 @@
+/*
+ANCHOR WEEK 2 MONDAY
+*/
+/*
+PROBLEM 1: Newton Second Law
+Pseudocode:
+
+STORE "percepatan" with 2
+STORE "massa" with 600
+
+CALCULATE "percepatan" times "massa"
+SET "resultan" value with calculation result
+
+DISPLAY "resultan"
+*/
+/*
+PROBLEM 2: Tahun Kabisat
+Pseudocode:
+
+STORE "tahun" with any number
+
+IF "tahun" mod 4 equals to 0
+    IF "tahun" mod 100 not equals to 0
+        DISPLAY "tahun kabisat"
+    ELSE
+    IF "tahun" mod 100 equals to 0
+        IF "tahun" mod 400 equals to 0
+            DISPLAY "tahun kabisat"
+ELSE
+    DISPLAY "bukan tahun kabisat"
+*/
+/*
+PROBLEM 3: Laundry Day
+Pseudocode:
+
+STORE "laundry" with 0
+
+WHILE "laundry" < 20
+    ADD "laundry" by 1
+
+DISPLAY "cuci sekarang!"
+*/
+/*
+PROBLEM 4: Periksa Kuku
+Pseudocode:
+
+STORE "kuku"
+STORE index "kuku" to 1
+
+WHILE "kuku" <= 40
+    IF "kuku" equals to "panjang"
+        DISPLAY "hukum!"
+    ELSE   
+        DISPLAY "puji!"
+    index "kuku" + 1
+*/
